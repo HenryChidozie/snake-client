bded5d0e22e9fc0b03dd763327f669033e779df7
HenryChidozie <chidoziechuks@hotmail.com>
2021-12-01 22:03:55 -0500

    Snake Client

---
+++ input.js
@@ -0,0 +1,42 @@
+let connection;
+
+const handleUserInput = (key) => {
+  // \u0003 maps to ctrl+c input
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  } else if (key === 'i') {
+    connection.write("Say: good luck!");
+  } else if (key === 'j') {
+    connection.write("Say: i'm winning");
+  } else if (key === 'k') {
+    connection.write("Say: i <3 red blox");
+  } else if (key === 'l') {
+    connection.write("Say: get out of my way!");
+  }
+};
+
+const setupInput = function(conn) {
+  connection = conn;
+  console.log(connection);
+  
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  //handleUserInput registeres keyboard inputs
+  stdin.on("data", handleUserInput);
+
+
+  return stdin;
+};
+
+module.exports = { setupInput };
